827408d68fbbd8e76c51d7a5441fe0f98380e9d8
Oksana <56485250+ok-skalko@users.noreply.github.com>
2020-10-25 16:26:37 +0200

    Add files via upload

--- yes.rb
+++ yes.rb
@@ -2,24 +2,26 @@ class Task2
 
   HASH = {'Yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234', yesss: :fg, try: 30, key: 'some value', ' yesterday1 '=> 34,' yesteryear '=> 2014}
 
+  def items
+    yes_elements.length
+  end
+
+  private
+
   def hash_keys
-    @hash_keys = HASH.keys
+    HASH.keys
   end
 
   def to_string
-    @to_string = hash_keys.map(&:to_s)
+    hash_keys.map(&:to_s)
   end
   
   def down_case
-    @down_case = to_string.map(&:downcase)
+    to_string.map(&:downcase)
   end
 
-  def select_elements
-    @select_elements = down_case.select { |str| str.start_with?('yes') }
-  end
-
-  def items
-    @items = select_elements.length
+  def yes_elements
+    down_case.select { |str| str.start_with?('yes') }
   end
 end
 
